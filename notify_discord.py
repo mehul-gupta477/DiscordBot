5413a1570948c50dccb8b9a8d660d72c78f5d2df
andewmark <amarkarian2021@gmail.com>
2025-06-27 15:33:01 -0400

    Adding Execution

--- notify_discord.py
+++ notify_discord.py
@@ -159,3 +159,6 @@ def main():
         comment_body = event["comment"]["body"]
         context_obj = event.get("issue") or event.get("pull_request", {})
         notify_comment_mention(comment_body, context_obj, user_map, webhook_url)
+
+if __name__ == "__main__":
+    main()

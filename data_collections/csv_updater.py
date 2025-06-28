8a1ccc2d63e18ec29854c1a39eba1ae1fc085d45
andewmark <amarkarian2021@gmail.com>
2025-06-27 23:08:42 -0400

    reverted commit & added error handling

--- data_collections/csv_updater.py
+++ data_collections/csv_updater.py
@@ -1,4 +1,4 @@
-import getEvents
+from getEvents import getEvents
 import os
 import pandas as pd
 from dotenv import load_dotenv
@@ -9,17 +9,17 @@ def items_to_csv(data: list[dict], path_to_file: str):
     Save items to a csv file
     """
     if data:
-        if not os.path.isfile(path_to_file):
-            raise ValueError("path_to_csv not found")
-        data_frame = pd.DataFrame(data)
-        data_frame.to_csv(path_to_file, index=False)
-        print(f"Items Successfully saved to {path_to_file}")
-    else:
-        print("No Items to Save")
+        try:
+            if not os.path.isfile(path_to_file):
+                raise ValueError("path_to_csv not found")
+            data_frame = pd.DataFrame(data)
+            data_frame.to_csv(path_to_file, index=False)
+            print(f"Items Successfully saved to {path_to_file}")
+        except Exception as e:
+            raise RuntimeError(f"Failed to save data to CSV: {e}") from e
 
 if __name__ == "__main__":
     load_dotenv("../.env")
     url = os.getenv("EVENTS_RSS_URL")
-    print(url)
-    data = getEvents.getEvents(url)
+    data = getEvents(url)
     items_to_csv(data, "runningCSV.csv")

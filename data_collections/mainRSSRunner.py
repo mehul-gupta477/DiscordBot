85f4cc06a832a5823d6f732551b481dccb3773d9
andewmark <amarkarian2021@gmail.com>
2025-07-25 23:30:47 -0400

    Revert "Merge branch 'feature/rss-jobs-internships' into stage"
    This reverts commit dff7bce46ade7e691a32e7ced98e373f6df47773, reversing
    changes made to d4a1cc3c5cc1b25173a077665b87528f80efa416.

--- data_collections/mainRSSRunner.py
+++ data_collections/mainRSSRunner.py
@@ -33,42 +33,12 @@ from dotenv import load_dotenv
 
 from .csv_updater import items_to_csv
 from .events import getEvents
-from .internships import getInternships
-from .jobs import getJobs
 
 
 def run_get_events(url, subType):
     if not url:
         raise ValueError(f"{subType}_RSS variable not set")
     data = getEvents(url, subType)
-    return data
-
-
-def run_jobs_RSS(url):
-    if not url:
-        raise ValueError("JOBS_RSS variable not set")
-    data = getJobs(url)
-    return data
-
-
-def run_internships_RSS(url):
-    if not url:
-        raise ValueError("INTERNSHIPS_RSS variable not set")
-    data = getInternships(url)
-    return data
-
-
-def run_jobs_RSS(url):
-    if not url:
-        raise ValueError("JOBS_RSS variable not set")
-    data = getJobs(url)
-    return data
-
-
-def run_internships_RSS(url):
-    if not url:
-        raise ValueError("INTERNSHIPS_RSS variable not set")
-    data = getInternships(url)
     return data
 
 
@@ -90,12 +60,6 @@ if __name__ == "__main__":
         url = os.getenv("OTHER_RSS")
     elif task_type == "CAREER_FAIR":
         url = os.getenv("CAREER_FAIR_RSS")
-    elif task_type == "JOBS":
-        url = os.getenv("JOBS_RSS")
-        data = run_jobs_RSS(url)
-    elif task_type == "INTERNSHIPS":
-        url = os.getenv("INTERNSHIPS_RSS")
-        data = run_internships_RSS(url)
     else:
         raise ValueError(f"Unsupported TASK_TYPE: {task_type}")
 

5cb7d738c863ef9bf4ec123910d32f70e598fd02
mgupta <rit2015025@iiita.ac.in>
2025-08-13 04:47:44 +0530

    ...

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -133,10 +133,6 @@ def run_all_tests(verbosity=2, pattern=None, modules=None, args=None):
     print("\033[95m" + "="*80 + "\033[0m")
     print("\033[95mDiscord Bot - Comprehensive Test Suite\033[0m")
     print("\033[95m" + "="*80 + "\033[0m")
-    """Run all test suites"""
-    print("\033[95m" + "="*80 + "\033[0m")
-    print("\033[95mDiscord Bot - Comprehensive Test Suite\033[0m")
-    print("\033[95m" + "="*80 + "\033[0m")
     
     if modules is None:
         modules = TEST_MODULES
